ac83ffdf57f87ede98b027ec62f9cfa7dd74f238
afaust65 <amandafaust65@gmail.com>
2026-07-24 13:49:09 -0400

    Initialize Supabase client with URL and key

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const supabaseUrl = "https://vayxtyuzdhmeaqwpqrro.supabase.co";
+const supabaseKey = "sb_publishable_Mk8BtDe-spuMgOiM_p1vDQ_6T6pz1LW";
 
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
